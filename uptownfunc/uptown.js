93e0d7888c1ab482ff5cb2fdcb0739c15f389770
Clint Carver <clintcarver3@gmail.com>
2019-10-08 23:15:02 -0600

    10/8

--- uptownfunc/uptown.js
+++ uptownfunc/uptown.js
@@ -11,3 +11,39 @@ var lyrics = ["This", "hit", "that", "ice", "cold",
 const reverseLyrics = lyrics.reverse();
     console.log(reverseLyrics.join(" "))
 
+
+
+
+//EVENT LISTENERS   
+
+// console.log(document)
+
+// const myBox = document.getElementById('box')
+// const tagBox = document.getElementsByTagName ('div')
+// const classBox = document.getElementsByClassName('boxes')
+
+//html collection is not an array. 
+
+const myBox = document.getElementById('box')
+const myText = document.getElementById('textBox')
+
+myBox.addEventListener('click', function(){  //this is a method
+    this.style.backgroundColor = 'blue'
+}
+
+myBox.addEventListener('change', function(){ 
+    myBox.style.backgroundColor = 'red'
+}
+
+document.addEventListener('keypress', function(e){
+    if(e.kley == 'y'){
+        myBox.style.backgroundColor = 'yellow'
+    }else if (e.key === 'g'){
+        myBox.style.backgroundColor = 'green'
+
+    }
+}
+
+
+
+//addEventListener //this is a function
