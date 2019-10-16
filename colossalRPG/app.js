2c4b74eccf88d773213d757b2786c3b110b7a344
Clint Carver <clintcarver3@gmail.com>
2019-10-15 19:23:56 -0600

    RPG Game & 10/15

--- colossalRPG/app.js
+++ colossalRPG/app.js
@@ -7,24 +7,41 @@ function Enemy (name, hp, ap){
     this.hp = hp;
     this.ap = ap;
 }
-const count = new Enemy('The Count', 53, 12)
-const dracula = new Enemy('Dracula', 100, 20)
-const nosferatu = new Enemy('Nosferatu', 600, 3)
-const enemies = [count, dracula, nosferatu]
+const witch = new Enemy('The Wicked Witch of the West', 10, 3)
+const lex = new Enemy('Lex Luthor', 100, 20)
+const tom = new Enemy('Tom Riddle', 400, 40)
+const enemies = [witch, lex, tom]
 
 function Hero (name, hp, ap){
     this.name = name;
     this.hp = hp;
     this.ap = ap;
 }
-const hero = new Hero('Superman', 500, 2)
-console.log('Hello how are you, welcome to the game')
-const name = ask.question('What is your name? ')
-console.log(`Welcome to the game ${name}, Your journey is about to begin`)
+
+const unicorn = new Hero('Unicorn', 100, 10)
+const monkey = new Hero('Monkey', 45, 5)
+const ninja = new Hero('Ninja', 65, 15)
+const heros = [unicorn, monkey, ninja]
+
+let inventory = []
+
+console.log('Hello and welcome! Your adventure begins now. You will encounter various enemines and your goal is find 3 keys to save the world. ')
+
+ask.question('Please choose a character to start your journey (push enter to continue) ')
+
+let charChoice = ask.keyIn('Would you like to be a (u) Unicorn, a (m) Monkey, a (n) Ninja or (q) Leave Game? ', {limit: 'umnq'} )
+    if(charChoice === 'u'){
+       console.log("You have selected Unicorn. Your secet weapon is your rainbow tail. Good Luck!")
+       walk()
+    }else if (charChoice === 'm'){
+        console.log("You have selected Monkey. Your secret weapon is your loud scream. Good luck!")
+    }else if (charChoice === 'n'){
+        console.log("You have selected Ninja. Your secret weapon. Good Luck!")
+}
 
 while(isAlive && !hasWon){
-    let choice = ask.keyIn('Would you like to (w) Walk, (i) Check Inventory, or (q) Leave Game? ', {limit: 'wiq'} )
-    if(choice === 'w'){
+    let choice = ask.keyIn('Would you like to (e) explore, (i) Check Inventory, or (q) Leave Game? ', {limit: 'eiq'} )
+    if(choice === 'e'){
         walk()
     }else if (choice === 'i'){
        printInventory()
@@ -35,12 +52,27 @@ while(isAlive && !hasWon){
 }
 
 function walk (){
-    let random = Math.floor(Math.random()*4)
+    let random = Math.floor(Math.random()*6)
     if(random === 3){
         enemyEncounter()
+    }else if(random === 5){
+        keyFound()
     }else{
         console.log('You continue walking safely')
     }
+}
+
+function keyFound(){
+    let randomKey = Math.floor(Math.random()*5)
+    //gets stored in inventory - once inventory has 3 keys the hero/player wins the game. 
+    if(randomKey === 5){
+        keyStored()
+    }
+}
+
+function keyStored(){
+    if(keyInventory === 3)
+        return hasWon
 }
 
 function enemyEncounter(){
@@ -60,3 +92,4 @@ function selectEnemy(){
     let random = Math.floor(Math.random()*enemies.length)
     return enemies[random]
 }
+
---
+++ exercises/arrayfilter/app.js
@@ -0,0 +1,9 @@
+function fiveAndGreaterOnly(arr) {
+    // your code here
+  }
+  // test
+  console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+
+  const fiveAndGreaterOnly = numbers.filter((numbers)=>{
+    return numbers.split 
+  }
