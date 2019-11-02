58545f0ae700ba19c0283cc5afb82dbbceb48be3
Clint Carver <clintcarver3@gmail.com>
2019-11-01 20:26:10 -0600

    11/1

---
+++ rlm-class/exercise-functions/app.js
@@ -0,0 +1,37 @@
+// function myFunc(sum1, sum2){
+//     return sum1 + sum2
+// }
+// var result = myFunc(2, 8)
+
+// console.log(result)
+
+// function threeParam(num1, num2, num3){
+//     if(num1 > num2 && num1 > num3){
+//         console.log('num1 is the largest')
+//     } else if (num2 > num1 && num2 > num3){
+//         console.log('num2 is the largest')
+//     } else if (num3 > num1 && num3 > num2)
+//         console.log('num3 is the largest')
+// }
+
+// console.log(threeParam(45, 78, 99))
+
+// function oneNum(num1){
+//     if((num1 % 2) === 0){
+//         console.log("even")
+//     } else if ((num1 % 2) !== 0){
+//         console.log("odd")
+//     }
+// }
+
+// console.log(oneNum(99))
+
+
+function string(str1){
+    if(str.length <= 20){
+        return str1 + str1
+    } else (str.length >= 20){\
+        return str1 / 2  
+    }
+}
+
---
+++ rlm-class/if-statements/app.js
@@ -0,0 +1,69 @@
+// if(3 > 5){
+//     console.log("is greater than") 
+// } else {
+//     console.log("is less than")
+// }
+
+
+// if("cat" === 3){
+//     console.log("is the length")
+// } else {
+//     console.log("is not the length")
+// }
+
+// if("cat" === "dog"){
+//     console.log("is the same")
+// } else ("cat" !== "dog"){
+//     console.log("not the same")
+// }
+
+
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+
+// if(person.age > 18){
+//     console.log("is allowed to go to the movie")
+// } else if (person.age < 18){
+//     console.log("is not allowed to go to the movie")
+// }
+
+// if(person.name = "B"){
+//     console.log("is allowed to go to the movie")
+// } else if (person.name = "B"){
+//     console.log("is not allowed to go to the movie")
+// }
+
+// if(person.name = "B" && person.age > 18){
+//     console.log("is allowed to go the movie")
+// } else if (person.name = "B" && person.age < 18){
+//     console.log("is not allowed to go the movie")
+// }
+
+// if(1 === "1"){
+//     console.log("loose")
+// } else if ( 1 == "1"){
+//     console.log("abstract")
+// } else if (1 !== "1"){
+//     console.log("not equal at all")
+// }
+
+// if(1 <= 2 && 2 === 4){
+//     console.log("yes")
+// } else if ( 1 <= 2 && 2 !== 4){
+//     console.log("no")
+// }
+
+// if("dog" === ""){
+//     console.log("dog is a string")
+// }
+
+for(let i = 0; i < 100; i++){
+    if((i % 2) == 0){
+        console.log([i])
+    } else if ((i % 2) == 0) {
+        console.log("Odd")
+    }
+
+}
