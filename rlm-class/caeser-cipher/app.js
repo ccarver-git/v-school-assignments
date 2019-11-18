b19057a209aca091e337b8e220679453c8d1c566
Clint Carver <clintcarver3@gmail.com>
2019-11-18 16:15:46 -0700

    rml practice

--- rlm-class/caeser-cipher/app.js
+++ rlm-class/caeser-cipher/app.js
@@ -7,12 +7,9 @@
 // one input - text
 // one input - number positions
 
-
-
-
-
 //PROBLEM SOLVING PRACTICE//
 
+//#1//
 // const numbers = []
 
 // function returnLargest(arr){
@@ -23,24 +20,53 @@
 // }
 // returnLargest()
 
-use Sort 
+//#2 Write a function that takes an array of words and a character and return each word that has that letter present.// 
+
+// function wordsCharacters(arr, str){
+//     const newArr = []
+//     for(i = 0; i < arr.length; i++){
+//         if(arr[i].includes(str)){
+//             newArr.push(arr[i])
+//         }
+//     }
+//     console.log(newArr)
+// }
+
+// wordsCharacters(["#3", "$$$", "C%4!", "Hey!"], "!");
+
 
 
 // function letterPresent(){
 
+//#3//
+// function isDivisible(num1, num2) {
+//   if (num1 % num2 === 0) {
+//     console.log("Yes, num1 is divisible " + true);
+//   } else {
+//     console.log("WRONG!");
+//   }
+// }
+// isDivisible();
+
+// - PRACTICE - using .toLowerCase(), .toUpperCase(), .slice(), .length and .concat(), make a capitalized version
+
+const frozenTwo = "Is the whole “putting us in mortal danger” going to be a regular thing?"
+const frozenQuote = "Here is a quote from Frozen II, "
+
+const twoStrings = frozenQuote.concat(frozenTwo);
+console.log(twoStrings);
+
+// const lowerCase = frozenTwo.toLowerCase();
+// console.log(lowerCase)
+
+// const upperCase = frozenTwo.toUpperCase();
+// console.log(upperCase)
+
+// const sliceFrozen = frozenTwo.slice(7, 12);
+// console.log(sliceFrozen)
+
+// console.log(frozenTwo.length)
 
 
 
-function isDivisible(num1, num2){
-    //takes two numbers and checks to see if the first is divisible by the second.
-    if(num1 % num2 === 0){
-        console.log("Yes, num1 is divisible" + true)
-    }else{
-        console.log("fireed")
-    }
-
-}
-isDivisible(2,2)
-
-
-
+// - Given: cannedMeats = ["chicken", "spam", "tuna", "clams"], how would we get get the index of "tuna"?
--- rlm-class/rpg-second-round/app.js
+++ rlm-class/rpg-second-round/app.js
@@ -1,32 +1,73 @@
-const readline = require("readline-sync");
+// const readline = require("readline-sync");
 
-let glassNotBroken = true;
-let gameWon = false;
-let basketCount = 0;
+// let gameLost = true;
+// let gameWon = false;
+// let basketCount = 0;
 
-console.log("Welcome to NBA Jam");
-const name = readline.question("What is your name? ");
-console.log(
-  `Welcome to the game ${name}, just like the arcade game the goal is to win or not get knockout by a broken backboard. First one to 5 points wins. You vs the CPU. Game on! `
-);
+// //GOOD GUYS//
+// const stockton = Hero John Stockton(shooting power, health power, )
+// const mitchell = Hero Donovan Mitchell
 
-gameStart()
+// //good guy function (){
 
-while(gameWon && !glassNotBroken){
-    if(basketCount === 5){
-        backboardBroken = false;
-        gameWon = true;
-        console.log("BOOMSHAKALAKA! Great shot! Game over! You win!.");
-    } else {
-        let choice = readline.keyIn("WOuld you like to (d)dribble the ball, pass the ball or shoot?")
-        // 
-    } if (choose _){
+// }
 
-    } else if (choose _){
+// //BAD GUYS//
+// const rodman = Enemy Dennis Rodman
+// const artest = Enemy Ron Artest 
 
+// // bad guy function(){
+
+// }
+
+
+// // GAME INTRODUCTION // 
+// console.log("Welcome to NBA Jam");
+// const name = readline.question("What is your name? ");
+// console.log(
+//   `Welcome to the game ${name}, just like the arcade game the goal is to win or not get knockout by a broken backboard. First one to 5 points wins. You vs the CPU. Game on! `
+// );
+
+// gameStart()
+
+// while(!gameWon && !gameLost){
+//     if(basketCount === 5){
+//         gameLost = false;
+//         gameWon = true;
+//         console.log("BOOMSHAKALAKA! Great shot! Game over! You win!.");
+//     } else {
+//         let choice = readline.keyIn("Would you like to (d)dribble the ball, or (s)shoot the ball?", {limit: "ds"});
+//         if (choose === "d"){
+//             dribble()
+//     } else if (choose === "s"){
+//             shoot()
+//         }
+//     }
+// }
+
+// // CHOOSE A PLAYER BEFORE YOU START THE GAME //
+// function gameStart(){
+//     ("Would you like to be the (j) John Stockton or (d) Donovan Mtichell? ");
+// }
+
+
+// // DRIBBLING THE BALL //
+// function dribble(){
+//     //random chance of loosing the ball
+// }
+
+// // SHOOTING THE BALL //
+// function shoot(){
+//     // based on skill level you might have to dribble closer to the basket before shooting
+// }
+
+function soundOfMusic(arr, str){
+    const newArr = []
+    for(let i = 0; i < favoriteThings.length; i++){
+        if(arr[i].includes(str))
+        newArr.push(arr[i])
     }
+    console.log(newArr)
 }
 
-function gameStart(){
-    //
-}
+soundOfMusic(["Raindrops on roses", "Whiskers on kittens", "Bright copper kettles", "Warm woollen mittens"], "o");
