60d05d0f088ade418c012296b15417b8dedc67a6
Clint Carver <clintcarver3@gmail.com>
2019-12-19 09:14:29 -0700

    princess-castle

---
+++ rlm-class/module2/princess/app.js
@@ -0,0 +1,65 @@
+class Player {
+  constructor(name) {
+    this.hasStar = false;
+    this.gameActive = true;
+    this.name = name;
+    this.totalCoins = 0;
+    this.status = "small";
+  }
+
+  gotHit() {
+    if (this.status === "small") {
+      this.gameActive = false;
+      console.log("You are dead");
+    } else if (this.status === "big") {
+      this.status = "small";
+    } else if (this.status === "powered up") {
+      this.status = "big"
+    }
+  }
+
+  gotPowerup() {
+      if ( this.status === "small") {
+          this.status = "big"
+          console.log("You grew big!");
+      } else if (this.status === "big") {
+          this.status = "powered up"
+          console.log("You got powered up!");
+      } else if ( this.status === "powered up"){
+          this.hasStar++
+          console.log("Congratulatoins! You got a star!");
+      }
+  }
+
+  addCoin() {
+    this.totalCoins++;
+  }
+  
+  print() {
+    console.log(`
+    Name: = ${this.name}
+    Status: ${this.status}
+    Total Coins: ${this.totalCoins}
+
+    `);
+  }
+}
+
+const player = new Player("mario")
+
+setInterval(() => {
+    
+},1000);
+
+while(player.gameActive) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+      player.gotHit();
+    } else if (randomNumber === 1) {
+      player.gotPowerup();
+    } else {
+      player.addCoin();
+    }
+    player.print()
+  } 
+  
